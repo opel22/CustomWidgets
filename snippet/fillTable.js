2764fbe4c3d9dfb9eaa350936885975ade10b513
oliver <oliver.pellaton@hotmail.com>
2020-02-17 19:07:00 +0100

    commit

--- snippet/fillTable.js
+++ snippet/fillTable.js
@@ -1,12 +1,27 @@
 function fillTable () {
     var myTable = document.getElementById("myTable");
     myTable.rows[5].cells[6].innerHTML = '23';   
+};
+
+function getJsonObject(cb){
+    // read text from URL location
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://opel22.github.io/CustomWidgets/snippet/year.json', true);
+    request.send(null);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+
+                   try {
+                     cb(JSON.parse(request.responseText);
+                   }catch(err) {
+                     cb(err);
+                   }
+        }
+    }
 }
 
-function yearLoad () {
-    var loadedData;
-    fetch("year.json")
-        .then(response => response.json())
-        .then(parsed => loadedData);
-console.log(loadedData);
+function yearLoad (function(object){
+    //Do what you want with the object here
+    console.log(object);
 };
