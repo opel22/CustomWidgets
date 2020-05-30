8488b7e4821918d8ad052f8aa772db0200c363a0
oliver <opel22@users.noreply.github.com>
2020-05-30 15:40:47 +0200

    commit

--- halloWelt/halloWelt.js
+++ halloWelt/halloWelt.js
@@ -1,13 +1,13 @@
 (function() { 
-/*    let ui5Script = document.createElement('script');
+    let ui5Script = document.createElement('script');
     ui5Script.src = 'https://sapui5.hana.ondemand.com/resources/sap-ui-core.js';
     ui5Script.async = false;
     document.head.appendChild(ui5Script);
-*/
+
 
     let template = document.createElement('template');
     template.innerHTML = `
-    <p>Test</p>
+    <body class="sapUiBody" id="content"></body>
     <style>
     :host {
         border-radius: 25px;
