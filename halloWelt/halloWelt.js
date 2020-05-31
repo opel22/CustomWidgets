d41ea4b66c10421264b4e3edff3485591b8815bd
oliver <opel22@users.noreply.github.com>
2020-05-31 10:39:46 +0200

    commit

--- halloWelt/halloWelt.js
+++ halloWelt/halloWelt.js
@@ -1,8 +1,10 @@
 (function() { 
     let ui5Script = document.createElement('script');
+    ui5Script.type = 'text/javascript';
     ui5Script.src = 'https://sapui5.hana.ondemand.com/resources/sap-ui-core.js';
-    ui5Script.async = false;
-    document.head.appendChild(ui5Script);
+    ui5Script.addEventListener(load, callback);
+    shadowRoot.appendChild(ui5Script);
+    //document.head.appendChild(ui5Script);
 
 
     let template = document.createElement('template');
@@ -18,15 +20,14 @@
     <body class="sapUiBody" id="content"></body> 
     `;
     
-    ui5Script.onload = () => 
-    customElements.define("com-sap-sample-hallowelt", class HalloWelt extends HTMLElement {
+    class HalloWelt extends HTMLElement {
         constructor() {
             super(); 
-            let shadowRoot = this.attachShadow({mode: "open"});
-            shadowRoot.appendChild(template.content.cloneNode(true));
+            this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot.appendChild(template.content.cloneNode(true));
 
         }
 
     });
-
+    customElements.define("com-sap-sample-hallowelt",  HalloWelt)
 })();
