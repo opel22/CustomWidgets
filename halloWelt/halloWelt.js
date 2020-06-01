3d4f35af2ccdd7d5631133d2ac8cb0ad45d8b29d
oliver <opel22@users.noreply.github.com>
2020-06-01 11:46:54 +0200

    commit

--- halloWelt/halloWelt.js
+++ halloWelt/halloWelt.js
@@ -1,16 +1,15 @@
 (function() { 
-    let _shadowRoot;
-    let tmpl = document.createElement("template");
-    tmpl.innerHTML = `
-    <style>
-    :host {
-        border-radius: 25px;
-        border-width: 4px;
-        border-color: black;
-        border-style: solid;
-        display: block;
-    } 
-    </style> 
+    let template = document.createElement("template");
+    template.innerHTML = `
+        <style>
+        :host {
+            border-radius: 25px;
+            border-width: 4px;
+            border-color: black;
+            border-style: solid;
+            display: block;
+        } 
+        </style> 
     `;
 
     class HalloWelt extends HTMLElement {
