6b614a5742ab87f94faa8d8a5f6e2f0c26832da2
oliver <opel22@users.noreply.github.com>
2020-07-16 13:16:50 +0200

    commit

--- newTile/newTile.js
+++ newTile/newTile.js
@@ -40,7 +40,7 @@
         }
         set value(newValue) {
             debugger;
-            this._shadowRoot.getElementById("styling_color").value = newValue;
+            //this._shadowRoot.getElementById("styling_color").value = newValue;
         }
     }
     customElements.define("com-sap-sample-newtile", NewTile);
