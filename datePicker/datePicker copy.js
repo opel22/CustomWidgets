23ff015ad6673930a25d74dcbc66fc5db1428def
oliver <opel22@users.noreply.github.com>
2020-08-02 12:32:09 +0200

    commit

---
+++ datePicker/datePicker copy.js
@@ -0,0 +1,100 @@
+(function() { 
+    let _shadowRoot;
+    let _oView;
+
+    let template = document.createElement("template");
+    template.innerHTML = `
+        <div id="ui5_content" name="ui5_content">
+            <slot name="content"></slot>
+        </div>
+
+        <script id="sap-ui-bootstrap"
+                src="https://sapui5.hana.ondemand.com/resources/sap-ui-core.js"
+                data-sap-ui-libs="sap.ui.core, sap.m, sap.ui.unified"
+                data-sap-ui-theme="sap_fiori_3"
+                data-sap-ui-async="true"
+                data-sap-ui-compatversion="edge"
+                data-sap-ui-xx-waitfortheme="init"></script>
+
+        <script id="oView" name="oView" type="ui5_content">                 
+        <mvc:View
+            controllerName="myView.Template"
+            xmlns:mvc="sap.ui.core.mvc"
+            xmlns="sap.m">
+            <DatePicker
+                id="DP10"
+                value="2015-11-23"
+                displayFormat="MM-y"
+                change="handleChange"
+                class="sapUiSmallMarginBottom"/>
+        </mvc:View>
+        </script>  
+    `;
+    //Test for github
+    class NewDatePicker extends HTMLElement {
+        constructor() {
+            super(); 
+            _shadowRoot = this.attachShadow({mode: "open"});
+
+            _shadowRoot.appendChild(template.content.cloneNode(true));
+            
+            this._props = {};
+            loadthis(this);            
+        }
+        onCustomWidgetBeforeUpdate(changedProperties) {
+            if ("designMode" in changedProperties) {
+                this._designMode = changedProperties["designMode"];
+            }
+        }
+        onCustomWidgetAfterUpdate(changedProperties) {
+
+        }
+
+        get dateValue() {
+            let sViewId = this.firstChild.getAttribute("sapui5viewid");
+            var oView = sap.ui.getCore().byId(sViewId); 
+            if(!oView){
+                return; 
+            }
+            return oView.byId("DRS4").getDateValue();
+        }
+
+        
+    }
+    customElements.define("ch-processpartner-sample-datepicker", NewDatePicker);
+
+    function loadthis(that) {
+        var that_ = that;
+      
+        let content = document.createElement('div');
+        content.slot = "content";
+
+        that_.appendChild(content);
+
+        sap.ui.getCore().attachInit(function() {
+            "use strict";
+
+            //### Controller ###
+            sap.ui.define([
+                "sap/ui/core/mvc/Controller"
+            ], function(Controller) {
+                "use strict";
+                
+                return Controller.extend("myView.Template", {
+                    //hier kämen die Methoden rein, Bsp. onButtonPress
+                });
+            });
+
+            //### THE APP: place the XMLView somewhere into DOM ###
+            _oView  = sap.ui.xmlview({
+                viewContent: jQuery(_shadowRoot.getElementById("oView")).html(),
+            });
+            
+            _oView.placeAt(content);
+
+            content.setAttribute("sapUi5ViewId",_oView.getId());
+
+        });
+    }
+
+})();
--- datePicker/datePicker.js
+++ datePicker/datePicker.js
@@ -9,26 +9,15 @@
         </div>
 
         <script id="sap-ui-bootstrap"
-                src="https://sapui5.hana.ondemand.com/resources/sap-ui-core.js"
-                data-sap-ui-libs="sap.ui.core, sap.m, sap.ui.unified"
-                data-sap-ui-theme="sap_fiori_3"
-                data-sap-ui-async="true"
-                data-sap-ui-compatversion="edge"
-                data-sap-ui-xx-waitfortheme="init"></script>
+            src="https://openui5.hana.ondemand.com/resources/sap-ui-core.js"
+            data-sap-ui-libs="sap.ui.core, sap.m, sap.ui.unified"
+            data-sap-ui-theme="sap_fiori_3"
+            data-sap-ui-async="true"
+            data-sap-ui-compatversion="edge"
+            data-sap-ui-xx-waitfortheme="init">
+        </script><body id="content" class="sapUiBody"></body>
 
-        <script id="oView" name="oView" type="ui5_content">                 
-        <mvc:View
-            controllerName="myView.Template"
-            xmlns:mvc="sap.ui.core.mvc"
-            xmlns="sap.m">
-            <DatePicker
-                id="DP10"
-                value="2015-11-23"
-                displayFormat="MM-y"
-                change="handleChange"
-                class="sapUiSmallMarginBottom"/>
-        </mvc:View>
-        </script>  
+        <script id="oView" name="oView" type="ui5_content"></script>  
     `;
     //Test for github
     class NewDatePicker extends HTMLElement {
@@ -71,30 +60,42 @@
 
         that_.appendChild(content);
 
-        sap.ui.getCore().attachInit(function() {
+        sap.ui.getCore().attachInit(() => sap.ui.require([
+            "sap/ui/core/Fragment",
+            "sap/ui/model/json/JSONModel",
+            "sap/ui/core/Core",
+          ], async (Fragment, JSONModel, Core) => {
             "use strict";
-
-            //### Controller ###
-            sap.ui.define([
-                "sap/ui/core/mvc/Controller"
-            ], function(Controller) {
-                "use strict";
-                
-                return Controller.extend("myView.Template", {
-                    //hier kämen die Methoden rein, Bsp. onButtonPress
-                });
+          
+            const control = await Fragment.load({
+              definition: `<DatePicker xmlns="sap.m" xmlns:core="sap.ui.core"
+                core:require="{ DateType: 'sap/ui/model/type/Date' }"
+                maxDate="{/maxDate}"
+                class="sapUiTinyMargin"
+                displayFormat="yyyy"
+                valueFormat="yyyy"
+                width="7rem"
+                value="{
+                  path: '/myYear',
+                  type: 'DateType',
+                  formatOptions: {
+                    pattern: 'yyyy',
+                    source: {
+                      pattern: 'yyyy'
+                    }
+                  },
+                  constraints: { maximum: 2030 }
+                }"
+              />`,
             });
-
-            //### THE APP: place the XMLView somewhere into DOM ###
-            _oView  = sap.ui.xmlview({
-                viewContent: jQuery(_shadowRoot.getElementById("oView")).html(),
-            });
-            
-            _oView.placeAt(content);
-
-            content.setAttribute("sapUi5ViewId",_oView.getId());
-
-        });
+          
+            Core.getMessageManager().registerObject(control, true);
+            control.setModel(new JSONModel({
+              myYear: new Date().getFullYear(), // current year, e.g. 2019
+              maxDate: new Date("2030-12-31") // control awaits a JS date object for maxDate
+            })).placeAt("content");
+          }));
+        
     }
 
 })();
